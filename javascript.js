b1d5030113e84a51e5a6a0eecbd7023526300a74
Asad Saeeduddin <masaeedu@gmail.com>
2015-10-17 12:42:32 -0400

    Updated user branch from Portal workspace (javascript)

--- javascript.js
+++ javascript.js
@@ -2,11 +2,88 @@ angular.module('PortalApp')
 
 .controller('facultywarsCtrl', ['$scope', '$http', '$q', 'facultywarsFactory', function ($scope, $http, $q,
 facultywarsFactory) {
+
+    // Widget Configuration
+    $scope.portalHelpers.config = {
+        // make 'widgetMenu.html' the template for the top right menu
+        "widgetMenu": "widgetMenu.html"
+    };
     
-    $scope.configuration
-    
-    $scope.riddle = "What has roots but does not grow?";
- 
+    // initialize the service
+    facultywarsFactory.init($scope);
+
+}])
+    // Factory maintains the state of the widget
+    .factory('facultywarsFactory', ['$http', '$rootScope', '$filter', '$q', function ($http, $rootScope,
+        $filter, $q) {
+        var initialized = {
+            value: false
+        };
+
+        // Your variable declarations
+        var loading = {
+            value: true
+        };
+        var insertValue = {
+            value: null
+        };
+        var links = {
+            value: null
+        };
+        var openDataExampleData = {
+            value: null
+        };
+        var dbData = {
+            value: null
+        };
+        var item = {
+            value: null
+        };
+        var sourcesLoaded = 0;
+
+        var init = function ($scope) {
+            if (initialized.value)
+                return;
+            initialized.value = true;
+
+            // Place your init code here:
+
+            // Get data for the widget
+            $http.get('/ImportantLinks/JSONSource').success(function (data) {
+                links.value = data;
+                sourceLoaded();
+            });
+
+            // OPEN DATA API EXAMPLE
+            $scope.portalHelpers.invokeServerFunction('getOpenData').then(function (
+                result) {
+                console.log('getopendata data: ', result);
+                openDataExampleData.value = result.data;
+                sourceLoaded();
+            });
+
+            $scope.portalHelpers.invokeServerFunction('getData').then(function (result) {
+                dbData.value = result;
+                sourceLoaded();
+            });
+        }
+
+        function sourceLoaded() {
+            sourcesLoaded++;
+            if (sourcesLoaded == 3)
+                loading.value = false;
+        }
+
+        return {
+            init: init,
+            loading: loading,
+            insertValue: insertValue,
+            links: links,
+            openDataExampleData: openDataExampleData,
+            dbData: dbData,
+            item: item
+        };
+
     }])
     // Custom directive example
     .directive('facultywarsDirectiveName', ['$http', function ($http) {
