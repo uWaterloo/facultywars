1b4205d17f849d3bf484e0e6928ef53978b4209a
hacktheverse <alyacarina@gmail.com>
2015-10-17 15:15:34 -0400

    Updated user branch from Portal workspace (javascript)

--- javascript.js
+++ javascript.js
@@ -4,6 +4,28 @@ angular.module('PortalApp')
         $http, $q,
         facultywarsFactory) {
 
+        $scope.oneAtATime = true;
+
+        $scope.groups = [{
+            title: 'Dynamic Group Header - 1',
+            content: 'Dynamic Group Body - 1'
+        }, {
+            title: 'Dynamic Group Header - 2',
+            content: 'Dynamic Group Body - 2'
+        }];
+
+        $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+        $scope.addItem = function() {
+            var newItemNo = $scope.items.length + 1;
+            $scope.items.push('Item ' + newItemNo);
+        };
+
+        $scope.status = {
+            isFirstOpen: true,
+            isFirstDisabled: false
+        };
+
         $scope.chosenRiddle = {};
 
         $scope.processUserAnswer = function(userAnswer) {
@@ -37,17 +59,20 @@ angular.module('PortalApp')
         $scope.portalHelpers.showView("main.html", 1);
     }])
     // Factory maintains the state of the widget
-    .factory('facultywarsFactory', ['$http', '$rootScope', '$filter', '$q', function($http, $rootScope, $filter, $q) {
+    .factory('facultywarsFactory', ['$http', '$rootScope', '$filter', '$q', function($http,
+        $rootScope, $filter, $q) {
         var init = function($scope) {
             // Place your init code here:
             $scope.portalHelpers.invokeServerFunction('seed').then(function(result) {
-                $scope.portalHelpers.invokeServerFunction('getRiddles').then(function(result) {
-                    var riddles = result;
-                    var riddle = riddles[parseInt(Math.random() * riddles.length)];
+                $scope.portalHelpers.invokeServerFunction('getRiddles').then(
+                    function(result) {
+                        var riddles = result;
+                        var riddle = riddles[parseInt(Math.random() *
+                            riddles.length)];
 
-                    $scope.chosenRiddle.question = riddle.question;
-                    $scope.chosenRiddle.id = riddle.id;
-                });
+                        $scope.chosenRiddle.question = riddle.question;
+                        $scope.chosenRiddle.id = riddle.id;
+                    });
             });
         }
 
